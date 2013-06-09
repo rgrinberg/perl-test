37aa84bedfaf7201821cc66ed0fb1b16b530c700
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-09 18:39:26 -0400

    url param and module uses

--- lib/oDesk/Parser/ProgrammingLanguages/Wikipedia.pm
+++ lib/oDesk/Parser/ProgrammingLanguages/Wikipedia.pm
@@ -1,4 +1,16 @@
 use strict;
 use warnings;
 package oDesk::Parser::ProgrammingLanguages::Wikipedia;
+use Math::Prime::TiedArray;
+use HTML::TreeBuilder::XPath;
+use LWP::UserAgent;
+use Moose;
+use namespace::autoclean;
+
+has url => (
+    is => 'ro',
+    default => sub {
+        'http://en.wikipedia.org/wiki/List_of_programming_languages'
+    },
+);
 1;

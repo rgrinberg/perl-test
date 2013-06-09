6d7ea4a1fe1b3b2df55bafce993ed65d247bac3a
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-09 18:41:08 -0400

    anagram solver

--- lib/oDesk/Parser/ProgrammingLanguages/Wikipedia.pm
+++ lib/oDesk/Parser/ProgrammingLanguages/Wikipedia.pm
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 package oDesk::Parser::ProgrammingLanguages::Wikipedia;
+use utf8;
 use Math::Prime::TiedArray;
 use HTML::TreeBuilder::XPath;
 use LWP::UserAgent;
@@ -13,6 +14,20 @@ has url => (
         'http://en.wikipedia.org/wiki/List_of_programming_languages'
     },
 );
+
+has char_encoding => (
+    is => 'ro',
+    default => sub {
+        my @primes;
+        tie @primes, 'Math::Prime::TiedArray';
+        my %mapping = ();
+        for (0..255) { # we actually don't need this many..
+            $mapping{chr $_} = $primes[$_];
+        };
+        return \%mapping;
+    },
+);
+
 sub get_data {
     my $self = shift;
     my $ua = LWP::UserAgent->new;
@@ -24,4 +39,45 @@ sub get_data {
     return @langs if wantarray();
     return \@langs;
 }
+
+# if $w1 is an anagram of $w2 then encode_anagram($w1) eq encode_anagram($w2)
+sub encode_anagram {
+    # every word has a unique representation as an anagram obtained by 
+    # mapping every character in the word to a different prime number
+    # and the signature is then the product of those primes.
+    # Uniqueness is guranteed by the prime factorization theorem.
+    my ($self, $str) = @_;
+    my $total = 1;
+    for (split //, $str) {
+        $total *= $self->char_encoding->{$_};
+    }
+    return $total;
+}
+
+# Performance in the worst case should be O(n * m) where 
+# n - the number of words analyzing in total
+# m - the average length of each word
+# Space complexity is also O(n * m)
+sub get_anagrams {
+    my $self = shift;
+    my @data = $self->get_data;
+    my %anagrams = (); # keep track of encodings map to anagrams
+    # mapping of encoding to a list of anagrams
+    my %words = ();
+    foreach my $lang (@data) {
+        my $enc = $self->encode_anagram($lang);
+        if (exists $words{$enc}) {
+            push @{$words{$enc}}, $lang;
+            $anagrams{$enc} = 1;
+        } else {
+            $words{$enc} = [$lang];
+        }
+    }
+    # get a list of all anagrams signatures that have more than 1 word
+    my @anagram_encodings = keys %anagrams;
+    # now extract all the words for every sig and join the list into 1
+    return [ map { @$_ } @words{@anagram_encodings} ];
+}
+
+__PACKAGE__->meta->make_immutable;
 1;

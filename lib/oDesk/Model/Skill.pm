e82dd1d825eada9000557ac2f2fd70c86c188ecf
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-10 03:22:17 -0400

    pod for skill.pm

--- lib/oDesk/Model/Skill.pm
+++ lib/oDesk/Model/Skill.pm
@@ -42,3 +42,52 @@ sub get_id_for_name {
 
 __PACKAGE__->meta->make_immutable;
 1;
+=pod
+
+=head1 NAME
+
+oDesk::Model::Skill
+
+=head1 SYNOPSIS
+
+  use oDesk::DB;
+  use ODesk::Model::Skill;
+
+  my $db = oDesk::DB->new;
+  my $model = oDesk::Model::Skill->new(db => $db);
+  
+  # print all skills
+  printf "$skill\n" foreach my $skill ($model->get_skill_names);
+
+
+=head1 INTRODUCTION
+
+class representing the model for contractor skills
+
+=head1 METHODS
+
+=head2 get_all_hashref 
+
+returns a hashref that maps all the skill id's in the database to their
+respective skill names
+
+=head2 get_skill_names 
+
+Returns an arrayref to all the skill names in scalar context. Just
+returns the array in array context
+
+=head2 get_name_for_id 
+
+$c->get_name_for_id($id) returns the name of the skill corresponding
+to id:$id
+
+=head2 get_id_for_name 
+
+$c->get_id_for_name($id) returns the id of the skill corresponding
+to name: $name
+
+=head1 AUTHOR
+
+Rudi Grinberg <rudi.grinberg@gmail.com>
+
+=cut

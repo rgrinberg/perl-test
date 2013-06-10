bd85775002ae444ca5267620ed25caafbba271e2
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-10 03:10:40 -0400

    contractor.pm pod

--- lib/oDesk/Model/Contractor.pm
+++ lib/oDesk/Model/Contractor.pm
@@ -104,3 +104,105 @@ sub get_common_skills {
 
 __PACKAGE__->meta->make_immutable;
 1;
+=pod
+
+=head1 NAME
+
+oDesk::Model::Contractor
+
+=head1 SYNOPSIS
+
+  use oDesk::DB;
+  use ODesk::Model::Contractor;
+
+  my $db = oDesk::DB->new;
+  my $model = oDesk::Model::Contractor->new(db => $db);
+  # create a contractor:
+  my $contractor_id = $model->create(
+    first_name  => 'John',
+    last_name   => 'Smith',
+    hourly_rate => '20.22',
+    country_id  => $country_id,
+  );
+
+  # now delete
+  $model->delete($contractor_id);
+
+  # count the number of contractors
+  my $total_countractors = scalar($model->get_all);
+
+=head1 INTRODUCTION
+
+class representing the contractors
+
+=head1 FIELDS
+
+=head2 id
+
+id of contractor
+
+=head2 first_name
+
+first name of contractor
+
+=head2 last_name
+
+last name of contractor
+
+=head2 hourly_rate
+
+hourly_rate of a contractor
+
+=head2 country_id
+
+the country id of the origin country of a contractor
+
+=head1 METHODS
+
+=head2 create
+
+$contractor->create(%contractor) creates a new contractor and returns its id.
+%contractor must contain the key value pairs corresponding to the fields.
+
+=head2 update
+
+updates the contractor record in the database using the current data in the
+object
+
+=head2 load
+
+$contractor->load($id) returns a contractor object corresponding to the
+contractor in the database with id $id.
+
+=head2 delete
+
+$contractor->delete($id) deletes the contractor with id $id from the database
+
+=head2 get_all
+
+$contractor->get_all() returns a list of all the contractors in the database.
+every element in the list is a contractor object
+
+=head2 get_skills
+
+$contrctor->get_skills returns a list of skill ids of the skills that
+the contractor posses
+
+=head2 add_skill
+
+$contractor->add_skill($skill_id) adds the skill corresponding to $skill_id 
+to the $contractor skillset
+
+=head2 delete_skill
+
+$contractor->delete_skill($skill_id) removes the skill corresponding to $skill_id
+from the $contractor's skill set.
+
+=head2 get_common_skills
+
+$contractor->get_common_skills($c1, $c2) returns a list of skill ids
+that the contractors $c1 and $c2 have in common
+
+Rudi Grinberg <rudi.grinberg@gmail.com>
+
+=cut
